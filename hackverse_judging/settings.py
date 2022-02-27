834cdf896e0b3447501f8219606908f14b5732fc
Aritra Sinha <aritrasinha.191ec108@nitk.edu.in>
2022-02-27 10:49:30 +0530

    Check for deployment bugs

--- hackverse_judging/settings.py
+++ hackverse_judging/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p1odeue5bwf2kx8#-9l%_z5#zzvrbt#(wft(l+k%)s8m1$-h$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*.herokuapp.com', '127.0.0.1', 'localhost']
 
